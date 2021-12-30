0c8f63180475b01c8b590f7150b146e717b4f065
Steve J. South(NamJungGu) <nowage@gmail.com>
2021-12-30 02:05:45 +0000

    update install.sh

--- 1.DeepLearningServingSystem/4.BuildSystem/install.sh
+++ 1.DeepLearningServingSystem/4.BuildSystem/install.sh
@@ -17,16 +17,17 @@ sudo apt install -y software-properties-common
 
 # # Security
 #
-x=$(cat /etc/ssh/sshd_config|grep PasswordAuthentication yes)
-[ ${#x} -eq 0 ]&& echo "PasswordAuthentication no" >> /etc/ssh/sshd_config;systemctl restart ssh
-#
-x=$(cat /etc/bash.bashrc|grep EDITOR)
-[ ${#x} -eq 0]&& echo "export EDITOR=vi">/etc/bash.bashrc
+# x=$(cat /etc/ssh/sshd_config|grep PasswordAuthentication yes)
+# [ ${#x} -eq 0 ]&& echo "PasswordAuthentication no" >> /etc/ssh/sshd_config;systemctl restart ssh
+# #
+# x=$(cat /etc/bash.bashrc|grep EDITOR)
+# [ ${#x} -eq 0]&& echo "export EDITOR=vi">/etc/bash.bashrc
 
-PS1="\[\e]0;\u@\h: \w\a\]\[\033[01;31m\]G\[\033[00m\] \[\033[01;34m\]\w \[\033[01;30m\]#\[\033[m\] "
+# PS1="\[\e]0;\u@\h: \w\a\]\[\033[01;31m\]G\[\033[00m\] \[\033[01;34m\]\w \[\033[01;30m\]#\[\033[m\] "
 
 # Info
-sudo hostnamectl set-hostname fg1
+[ ! $(echo $1) ]&& echo "Usage : . install.sh {hostname}"
+[   $(echo $1) ]&& sudo hostnamectl set-hostname $1
 
 # Install
 ## Python
@@ -43,26 +44,25 @@ export WORKON_HOME=~/.virtualenvs
 EOF
 fi
 #### Jupyter
-sudo pip3 install jupyter
+pip3.7 install jupyter
 sudo apt-get remove -y python-pexpect python3-pexpect
-sudo pip3  install --upgrade pexpect
+pip3.7  install --upgrade pexpect
 echo "yes"|jupyter notebook --generate-config
 sudo pip3 install https://github.com/ipython-contrib/jupyter_contrib_nbextensions/tarball/master
 sudo jupyter contrib nbextension install --user
-sudo python3.7 -m pip3 install RISE
+pip3.7 install RISE
 sudo jupyter-nbextension install rise --py --sys-prefix
-sudo python3.7 -m pip3 install pandas matplotlib seaborn pillow scipy
-sudo python3.7 -m pip3 install cython numpy
-sudo python3.7 -m pip3 install opencv-python
+pip3.7 install pandas matplotlib seaborn pillow scipy
+pip3.7 install cython numpy
+pip3.7 install opencv-python
 
 pwd=$(pwd)
 
 
 ### Python
-ln -s /usr/bin/python3 /usr/bin/python
-ln -s /usr/bin/pip3 /usr/bin/pip
-python -m pip install --upgrade pip
+ln -s /usr/bin/python3.7 /usr/bin/python
+ln -s /usr/bin/pip3.7 /usr/bin/pip
+python3.7 -m pip install --upgrade pip
 
 cd $pwd
-. VagrantInstall.sh
-. installTensorflow.sh
+# . installTensorflow.sh
