4dc835cf9bb6f12d07a1aa492ae6ff1e2059019f
root <nackchun@gmail.com>
2022-01-05 19:24:01 +0000

    fix location input_fn from main to cnn

--- 2.AITensorflowTraining/5.EstimatorBasedMultiWorkerTraining/main.py
+++ 2.AITensorflowTraining/5.EstimatorBasedMultiWorkerTraining/main.py
@@ -1,4 +1,3 @@
-import tensorflow_datasets as tfds
 import tensorflow as tf
 from multiprocessing import util
 
@@ -38,25 +37,6 @@ def write_filepath(filepath, task_type, task_id, cluster_spec):
 
 checkpoint_dir = os.path.join(util.get_temp_dir(), 'ckpt')
 
-
-def input_fn(mode, input_context=None):
-  datasets, info = tfds.load(name='mnist',
-                                with_info=True,
-                                as_supervised=True)
-  mnist_dataset = (datasets['train'] if mode == tf.estimator.ModeKeys.TRAIN else
-                   datasets['test'])
-
-  def scale(image, label):
-    image = tf.cast(image, tf.float32)
-    image /= 255
-    return image, label
-
-  if input_context:
-    mnist_dataset = mnist_dataset.shard(input_context.num_input_pipelines,
-                                        input_context.input_pipeline_id)
-  return mnist_dataset.map(scale).cache().shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
-
-
 # os.environ['TF_CONFIG'] = json.loads("{ 'cluster': { 'worker': ['g1:12345', 'g2:23456'] },'task': {'type': 'worker', 'index': 0} }")
 
 strategy = tf.distribute.experimental.MultiWorkerMirroredStrategy()
@@ -67,6 +47,6 @@ classifier = tf.estimator.Estimator(
     model_fn=cnn.model_fn, model_dir='/tmp/multiworker', config=config)
 tf.estimator.train_and_evaluate(
     classifier,
-    train_spec=tf.estimator.TrainSpec(input_fn=input_fn),
-    eval_spec=tf.estimator.EvalSpec(input_fn=input_fn)
+    train_spec=tf.estimator.TrainSpec(input_fn=cnn.input_fn),
+    eval_spec=tf.estimator.EvalSpec(input_fn=cnn.input_fn)
 )
