b8dbc60b9f34df2089150646775983997920c7d8
NackchunA <nackchun@gmail.com>
2022-01-19 10:50:46 +0900

    add bento files

---
+++ 4.AIService/3.BentoML/bento_packer.py
@@ -0,0 +1,12 @@
+
+# import the IrisClassifier class defined above
+from bento_service import IrisClassifier
+
+# Create a iris classifier service instance
+iris_classifier_service = IrisClassifier()
+
+# Pack the newly trained model artifact
+iris_classifier_service.pack('model', clf)
+
+# Save the prediction service to disk for model serving
+saved_path = iris_classifier_service.save()
